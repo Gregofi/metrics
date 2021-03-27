6b408987494f65ffc7cfad0dd1fe5c52a3c47401
Filip Gregor <gregofi1@fit.cvut.cz>
2021-03-27 18:46:25 +0100

    Added NPATH implementation

--- modules/NPathVisitor.cpp
+++ modules/NPathVisitor.cpp
@@ -3,3 +3,123 @@
 //
 
 #include "NPathVisitor.hpp"
+#include "Logging.hpp"
+
+using namespace clang;
+
+NPathVisitor::NPathVisitor(clang::ASTContext *ctx) : AbstractVisitor(ctx)
+{
+
+}
+
+void NPathVisitor::CalcMetrics(clang::Decl *decl)
+{
+    auto *body = llvm::dyn_cast<CompoundStmt>(decl->getBody());
+    StmtNPathVisitor visitor;
+    visitor.Visit(body);
+    metrics.push_back({"NPATH", visitor.GetCount()});
+}
+
+/*-----------------------------------------------------------------------------------*/
+
+void StmtNPathVisitor::VisitStmt(const Stmt *stmt)
+{
+    LOG("Visiting statement: " << stmt->getStmtClassName());
+    int result = 1;
+    for(const auto & x : stmt->children())
+    {
+        Visit(x);
+        result *= count;
+    }
+    count = result;
+
+}
+
+void StmtNPathVisitor::VisitCompoundStmt(const clang::CompoundStmt *stmt)
+{
+    int result = 1;
+    for(const auto &x : stmt->children())
+    {
+        Visit(x);
+        result *= count;
+    }
+    count = result;
+}
+
+void StmtNPathVisitor::VisitExpr(const clang::Expr *stmt)
+{
+    LOG("Visiting expr: " << stmt->getStmtClassName());
+    int result = 0;
+    for(const auto & x : stmt->children())
+    {
+        Visit(x);
+        result *= count;
+    }
+    count = result;
+}
+
+void StmtNPathVisitor::VisitDoStmt(const clang::DoStmt *stmt)
+{
+    int result = 1;
+    Visit(stmt->getCond());
+    result += count;
+    Visit(stmt->getBody());
+    result += count;
+    count = result;
+}
+
+void StmtNPathVisitor::VisitWhileStmt(const clang::WhileStmt *stmt)
+{
+    int result = 1;
+    Visit(stmt->getCond());
+    result += count;
+    Visit(stmt->getBody());
+    result += count;
+    count = result;
+}
+
+void StmtNPathVisitor::VisitSwitchStmt(const clang::SwitchStmt *stmt)
+{
+    int result = 1;
+    Visit(stmt->getCond());
+    for(const clang::SwitchCase *c = stmt->getSwitchCaseList(); c != nullptr; c = c->getNextSwitchCase() )
+    {
+        Visit(c);
+        result += count;
+    }
+    result += 1;
+}
+
+void StmtNPathVisitor::VisitIfStmt(const clang::IfStmt *stmt)
+{
+    LOG("Visiting statement: " << stmt->getStmtClassName());
+    int result = 1 - (stmt->getElse() != nullptr);
+    Visit(stmt->getCond());
+    result += count;
+    LOG("Visited expr: " << count);
+    Visit(stmt->getThen());
+    result += count;
+    LOG("Visited then: " << count);
+
+    if(decltype(stmt->getElse()) e = stmt->getElse())
+    {
+        Visit(e);
+        result += count;
+        LOG("Visited else: " << count);
+    }
+
+    count = result;
+}
+
+void StmtNPathVisitor::VisitCatch(const clang::CXXCatchStmt *stmt)
+{
+    int result = 1;
+}
+
+void StmtNPathVisitor::VisitTry(const clang::CXXTryStmt *stmt)
+{
+    int result = 1;
+}
+
+
+
--- modules/NPathVisitor.hpp
+++ modules/NPathVisitor.hpp
@@ -5,10 +5,49 @@
 #ifndef METRICS_NPATHVISITOR_HPP
 #define METRICS_NPATHVISITOR_HPP
 
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/AST/ASTContext.h"
 
-class NPathVisitor
+#include "AbstractVisitor.hpp"
+#include "clang/AST/StmtVisitor.h"
+
+class StmtNPathVisitor : public clang::ConstStmtVisitor<StmtNPathVisitor>
 {
+public:
+    void VisitStmt(const clang::Stmt *stmt);
+    void VisitCompoundStmt(const clang::CompoundStmt *stmt);
+    void Visit(const clang::Stmt *stmt) { ConstStmtVisitor<StmtNPathVisitor>::Visit(stmt); }
+    void VisitIfStmt(const clang::IfStmt *stmt);
+    void VisitSwitchStmt(const clang::SwitchStmt *stmt);
+    void VisitWhileStmt(const clang::WhileStmt *stmt);
+    void VisitDoStmt(const clang::DoStmt *stmt);
+    void VisitExpr(const clang::Expr *stmt);
+    void VisitTry(const clang::CXXTryStmt *stmt);
+    void VisitCatch(const clang::CXXCatchStmt *stmt);
+    int GetCount() const { return count; }
 
+protected:
+    int count = 0;
+};
+
+class NPathVisitor : public clang::RecursiveASTVisitor<NPathVisitor>, public AbstractVisitor
+{
+public:
+    NPathVisitor(clang::ASTContext *ctx);
+    virtual ~NPathVisitor() override = default;
+    virtual void CalcMetrics(clang::Decl *decl) override;
+//    int HandleStmt(clang::IfStmt *stmt);
+//    int HandleStmt(clang::WhileStmt *stmt);
+//    int HandleStmt(clang::DoStmt *stmt);
+//    int HandleStmt(clang::ForStmt *stmt);
+//    int HandleStmt(clang::SwitchStmt *stmt);
+//    int HandleStmt(clang::ConditionalOperator *stmt);
+//    int HandleStmt(clang::Expr *stmt);
+//    int HandleStmt(clang::CompoundStmt);
 };
 
 
