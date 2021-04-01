39be436fcc711eb3ed0577822ff9cee848a9b246
Filip Gregor <gregofi1@fit.cvut.cz>
2021-04-01 19:49:10 +0200

    Added more tests

--- test/ChidKem.cpp
+++ test/ChidKem.cpp
@@ -55,13 +55,7 @@ const char *members = R"(
 class a
 {
 public:
-    int foo()
-    {
-        int tmp = a * b;
-        tmp += a;
-        tmp += a * b;
-        return tmp;
-    }
+    int foo();
 
     int bar()
     {
@@ -77,6 +71,26 @@ public:
     }
 private:
     int a{}, b{}, c{};
+};
+
+int a::foo()
+{
+    int tmp = a * b;
+    tmp += a;
+    tmp += a * b;
+    return tmp;
+}
+)";
+
+const char *cohesion = R"(
+class a
+{
+public:
+    int a, b, c, d;
+    void f1() { a = 1; b = 1; }
+    void f2() { a = 1; }
+    void f3() {d = 3; }
+    void f4() {c = 5; }
 };
 )";
 
@@ -140,16 +154,35 @@ int MemberAccessTest()
     auto fnames = v.names.func_names;
     auto vis = v.vis;
 
-    ASSERT_EQ(vis.GetConstClass(cnames["a"]).functions[0].size(), 2);
-    ASSERT_EQ(vis.GetConstClass(cnames["a"]).functions[1].size(), 3);
-    ASSERT_EQ(vis.GetConstClass(cnames["a"]).functions[2].size(), 1);
+    ASSERT_EQ(vis.GetConstClass(cnames["a"]).functions[2].size(), 2);
+    ASSERT_EQ(vis.GetConstClass(cnames["a"]).functions[0].size(), 3);
+    ASSERT_EQ(vis.GetConstClass(cnames["a"]).functions[1].size(), 1);
+
+    ASSERT_EQ(vis.LackOfCohesion(cnames["a"]), 0);
     return 0;
 };
 
+int LackOfCohesionTest()
+{
+    auto v = Eval(cohesion);
+    auto cnames = v.names.class_names;
+    auto fnames = v.names.func_names;
+    auto vis = v.vis;
+
+    ASSERT_EQ(vis.LackOfCohesion(cnames["a"]), 4);
+    return 0;
+}
+
 int main()
 {
+    ASSERT_EQ(ChidKemVisitor::Similiar({1,2,3}, {2,5,6}), true);
+    ASSERT_EQ(ChidKemVisitor::Similiar({1,2,3}, {0,3,6}), true);
+    ASSERT_EQ(ChidKemVisitor::Similiar({1,2,4}, {3,5}), false);
+    ASSERT_EQ(ChidKemVisitor::Similiar({}, {1}), false);
+
     TEST(BasicCasesTest);
     TEST(InheritanceChainTest);
     TEST(MemberAccessTest);
+    TEST(LackOfCohesionTest);
     return 0;
 }
