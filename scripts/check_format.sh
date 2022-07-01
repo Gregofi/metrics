d0cc7d38c59334ad844bd89ea23fb3160202e999
Filip Gregor <filip.gregor98@gmail.com>
2022-07-01 16:18:56 +0200

    Return error code if some file is not properly formatted

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+failed=0
 for f in `find src include -name "*.cpp" -o -name "*.hpp"`; do
     clang-format --dry-run -Werror $f
+    if [[ $? -ne 0 ]]; then
+        failed=1
+    fi
 done
+
+exit ${failed}
