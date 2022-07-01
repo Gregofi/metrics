94bc3815246f9ab5c95087fc84a630176ca195ec
Filip Gregor <filip.gregor98@gmail.com>
2022-07-01 17:03:31 +0200

    Fix githook install

--- scripts/install_hooks.sh
+++ scripts/install_hooks.sh
@@ -3,5 +3,5 @@
 GIT_DIR=$(git rev-parse --git-dir)
 
 echo "Installing hooks..."
-ln -s scripts/pre-commit.bash $GIT_DIR/hooks/pre-commit
+ln -sf ../../scripts/pre-commit.sh $GIT_DIR/hooks/pre-commit
 echo "Done"!
